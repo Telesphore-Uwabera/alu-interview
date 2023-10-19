a109cda2f09237ad094508a43204e6e080754874
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-10-19 11:29:34 +0200

    :

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,60 +1,38 @@
 #!/usr/bin/python3
+
 def pascal_triangle(n):
     """
-    Generate Pascal's triangle up to a specified height.
+    Generate Pascal's Triangle of depth n as a list of lists.
 
-    Args:
-        n (int): The height of the Pascal's triangle.
-
-    Returns:
-        list: A list of lists representing Pascal's triangle.
-
-    If n is less than or equal to 0, an empty list is returned.
+    :param n: The depth of Pascal's Triangle.
+    :type n: int
+    :return: A list of lists representing Pascal's Triangle.
+    :rtype: list of lists of integers
     """
     if n <= 0:
         return []
 
     triangle = []
     for i in range(n):
-        row = [1]  # The first element in each row is always 1
-        if triangle:
-            last_row = triangle[-1]
+        if i == 0:
+            triangle.append([1])
+        else:
+            prev_row = triangle[i - 1]
+            new_row = [1]
             for j in range(1, i):
-                # Calculate the middle elements in the row based on the previous row
-                row.append(last_row[j - 1] + last_row[j])
-            row.append(1)  # The last element in each row is always 1
-        triangle.append(row)
+                new_row.append(prev_row[j - 1] + prev_row[j])
+            new_row.append(1)
+            triangle.append(new_row)
 
     return triangle
 
 def print_triangle(triangle):
     """
-    Print Pascal's triangle.
-
-    Args:
-        triangle (list): A list of lists representing Pascal's triangle.
-
-    Prints each row of Pascal's triangle.
+    Print the Pascal's Triangle.
     """
     for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row]))
+        print("[{}]".format(",".join(map(str, row)))
 
 if __name__ == "__main__":
-    # Correct output for n = 5
     print_triangle(pascal_triangle(5))
-
-    # Correct output for n = 1
-    print_triangle(pascal_triangle(1))
-
-    # Correct output for n = 0
-    print_triangle(pascal_triangle(0))
-
-    # Correct output for n = 10
-    print_triangle(pascal_triangle(10))
-
-    # Correct output for n = 100
-    # Note: Printing a large Pascal's triangle may result in a very long output.
-    # It's not practical to print the entire triangle for such a large value of n.
-    # You can verify the correctness of the result in other ways.
-    print_triangle(pascal_triangle(100))
 
